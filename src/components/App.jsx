d094b68eb18b8864cc7bec770fc54acc7618ff42
Volodymyr Ok <v.shark.t@gmail.com>
2022-11-21 15:53:27 +0200

    added startup files

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    );
+  }
+}
